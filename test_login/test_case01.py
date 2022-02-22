a18c58691679abddb5aaf6efcafb147ed8af3c9f
fushaoshan <317024297@qq.com>
2022-02-22 17:52:27 +0800

    case1

--- test_login/test_case01.py
+++ test_login/test_case01.py
@@ -3,20 +3,22 @@
 # author： fushaoshan
 # datetime： 2022/2/22 10:55 上午
 import pytest
+import allure
 
+@allure.feature('test_module_01')
 class Test_wrongLogin:
 
-    @pytest.mark.run(ordering=1)
+    @allure.story("1==1")
     def test_C001001(self):
         print('\n用例C001001')
         assert 1 == 1
 
-    @pytest.mark.run(ordering=1)
+    @allure.story("2==2")
     def test_C001002(self):
         print('\n用例C001002')
         assert 2 == 2
 
-    @pytest.mark.run(ordering=1)
+    @allure.story("3==3")
     def test_C001003(self):
         print('\n用例C001003')
         assert 3 == 3
