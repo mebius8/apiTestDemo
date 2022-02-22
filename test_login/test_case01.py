b6d03eff9d0cfcc57b1b46ca8502f964f647169c
fushaoshan <317024297@qq.com>
2022-02-22 10:58:49 +0800

    case1

---
+++ test_login/test_case01.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author： fushaoshan
+# datetime： 2022/2/22 10:55 上午
+
+class Test_wrongLogin:
+
+    def test_C001001(self):
+        print('\n用例C001001')
+        assert 1 == 1
+
+    def test_C001002(self):
+        print('\n用例C001002')
+        assert 2 == 2
+
+    def test_C001003(self):
+        print('\n用例C001003')
+        assert 3 == 2
